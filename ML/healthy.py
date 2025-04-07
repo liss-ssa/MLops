3ea80a27dc9bc95478eb6a9deadcf0fc9ac0b555
liss-ssa <167419275+liss-ssa@users.noreply.github.com>
2025-04-07 19:56:07 +0500

    Update healthy.py

--- ML/healthy.py
+++ ML/healthy.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 if __name__ == "__main__":
-    url = "http://127.0.0.1:5003/invocations"
+    url = "http://127.0.0.1:5000/invocations"
     headers = {"Content-Type": "application/json"}
     data = json.dumps({"inputs": [[-1.75938045, -1.2340347, -1.41327673, 0.76150439, 2.20097247, -0.10937195, 0.58931542, 0.1135538]]})
 
