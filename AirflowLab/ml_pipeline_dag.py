88c0e6a0830b0a0571c6eae4459aa410458736dd
liss-ssa <167419275+liss-ssa@users.noreply.github.com>
2025-05-29 16:20:59 +0500

    Update ml_pipeline_dag.py

--- AirflowLab/ml_pipeline_dag.py
+++ AirflowLab/ml_pipeline_dag.py
@@ -13,18 +13,41 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+import pandas as pd
+
+def load_and_preprocess(data_path):
+    df = pd.read_csv(data_path)
+    # Предобработка данных
+    df = df.dropna()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.drop(columns=['Adj Close'])
+    X = df.drop(columns=['Close'])
+    y = df['Close']
+    return X, y
+
+import os
+
 def load_data():
-    from load_data import load_and_preprocess
-    X, y = load_and_preprocess("Tesla.csv")
-    return {'X': X.to_dict(), 'y': y.to_dict()}
+    try:
+        dag_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_path = os.path.join(dag_dir, "Tesla.csv")
+        X, y = load_and_preprocess(csv_path)
+        
+        X['Date'] = X['Date'].dt.strftime('%Y-%m-%d %H:%M:%S')
+        
+        return {'X': X.to_dict(), 'y': y.to_dict()}
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        raise
 
 def train_model(**kwargs):
     ti = kwargs['ti']
     data = ti.xcom_pull(task_ids='load_data')
     
-    import pandas as pd
     X = pd.DataFrame.from_dict(data['X'])
     y = pd.Series(data['y'])
+    
+    X['Date'] = pd.to_datetime(X['Date'])
     
     from train_model import train_and_evaluate
     rmse, mae, r2 = train_and_evaluate(X, y)
